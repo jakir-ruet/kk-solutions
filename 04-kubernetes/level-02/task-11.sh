952a682395926c0bff7dcfbfee4b41223a56cba6
jakir-ruet <jakir.ruet.bd@gmail.com>
2025-09-13 06:44:34 +0600

    level 2, task 11 done

--- 04-kubernetes/level-02/task-11.sh
+++ 04-kubernetes/level-02/task-11.sh
@@ -8,9 +8,13 @@ kubectl get deployment
 kubectl describe deployment
 kubectl get pvc,pv
 
+kubectl get pod
 
+# Go to `/app/index.php` inside `httpd-php-container`
+kubectl exec -it lamp-wp-56c7c454fc-rmmc7 -c httpd-php-container -- /bin/bash
+cd /app/ # Should show something like `index.php`
 
-sudo vi /app/test-db.php inside `httpd-php-container`
+sudo vi /app/index.php
 
 <?php
     // Fetch environment variables
@@ -45,3 +49,37 @@ sudo vi /app/test-db.php inside `httpd-php-container`
     // Close connection
     mysqli_close($connect);
 ?>
+
+chown apache:apache index.php
+chmod 644 index.php
+php /app/index.php # Should show something like 'Connected successfully'
+
+kubectl get svc lamp-service
+# Should show something like 80:30008/TCP
+
+kubectl get svc lamp-service # check node port
+curl http://172.17.0.2:30008/ # 172.17.0.2 is CLUSTER-IP
+
+# if okay then done
+# if not okay, then
+
+kubectl exec -it lamp-wp-56c7c454fc-vt2dd -c httpd-php-container -- netstat -tlnp | grep httpd
+
+# # Should show something like
+# tcp        0      0 :::443                  :::*                    LISTEN      114/httpd
+# tcp        0      0 :::80                   :::*                    LISTEN      114/httpd
+
+kubectl get svc lamp-service -o yaml
+kubectl edit svc lamp-service
+# Now hit 'App' button right conner of panel, it will works.
+# Should show something like 'Connected successfully'
+
+# Optional
+kubectl get svc lamp-service -o yaml > lamp-service-backup.yaml
+kubectl apply -f lamp-service-backup.yaml
+
+
+
+
+
+
